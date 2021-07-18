50d8518f3b200ae9fc01fcdad334d1c3c689e966
Siyeol Choi <33966473+siyeol@users.noreply.github.com>
2021-07-19 02:22:31 +0900

    moviepy to ffmpeg

--- mfcc/mfcc_test.py
+++ mfcc/mfcc_test.py
@@ -2,6 +2,10 @@ import matplotlib.pyplot as plt
 import librosa.display
 import librosa
 import numpy as np
+import moviepy.editor as mp
+
+#https://stackoverflow.com/questions/65065501/trim-audio-file-using-python-ffmpeg
+#https://stackoverflow.com/questions/55550116/saving-audio-from-mp4-as-wav-file-using-moviepy-audiofile
 
 # mp.VideoFileClip(r"WJ_test.mp4").subclip(0,300).audio.write_audiofile("sample.wav")
 
@@ -9,8 +13,20 @@ import numpy as np
 #
 # command = "ffmpeg -i {} -ab 160k -ac 2 -ar 44100 -vn {}".format("WJ_test.mp4", "sample.wav")
 # subprocess.call(command, shell=True)
+# mp.AudioFileClip('SampleAudio2.wav').subclip(0,120).write_audiofile("temp.wav")
 
-path = 'SampleAudio1.wav'
+# print(type(mp.AudioFileClip('SampleAudio2.wav').subclip(0,120)))
+
+import ffmpeg  
+
+audio_input = ffmpeg.input('SampleAudio2.wav')
+audio_cut = audio_input.audio.filter('atrim', duration=150)
+audio_output = ffmpeg.output(audio_cut, 'out.mp3')
+print(type(audio_cut), type(audio_output))
+ffmpeg.run(audio_output)
+
+# path = 'temp.wav'
+path = 'SampleAudio2.wav'
 sample_rate=16000
 
 x = librosa.load(path,sample_rate)[0]
@@ -24,7 +40,7 @@ len = librosa.get_duration(x)
 silence = np.count_nonzero(abs(delta2_mfcc) < 2)
 size = delta2_mfcc.size
 rate = 100 - silence / size * 100
-print(size, silence,"\n", rate, "%")
+print(size, silence,"\n", rate, "%", '\n')
 
 
 
@@ -32,8 +48,8 @@ speakIndex = np.where(abs(delta2_mfcc)>5)[1]
 
 secIndex = speakIndex*len/size
 secIndexUni = np.unique(secIndex.astype(int))
-# print(speakIndex)
-print(np.unique(secIndex.astype(int)))
+print(speakIndex)
+print(np.unique(secIndex.astype(int)), '\n')
 # print(secIndexUni[0], secIndexUni[2], secIndexUni[3], secIndexUni[4], secIndexUni[5])
 
 toggle = True
@@ -42,7 +58,7 @@ toggle = True
 #     if i-6&i-5&i-4&i-3&i-2&i-1&i&i+1&i+2&i+3&i+4&i+5&i+6 in speakIndex :
 #         if Toggle == True :
 #             speakCount+=1
-#             print(i, "frame , ",i*len/size, "초")
+#             print(i, "frame , ",i*len/size, "")
 #             Toggle = False
 #     else :
 #         Toggle = True
@@ -50,25 +66,34 @@ toggle = True
 temp=0
 speakCount=0
 arrLen = secIndexUni.size
+
+speakLen = 0
 for j in secIndexUni:
     if (j-2 in secIndexUni) & (j-1 in secIndexUni) & (j in secIndexUni):
         if temp == 0:
             speakCount=speakCount+1
             temp = 1
             print(j)
+        else:
+            print(" ")
+            speakLen+=1
     else :
         temp = 0
 
+speakLen = speakLen + speakCount*3
 
-
-print("this is the number of speaks" , speakCount)
+print("Number he/she has spoken is : " , speakCount, "times")
+print("Time he/she has spoken is : " , speakLen, "sec")
 
 # print(delta2_mfcc[0][8111])
 
 # y = librosa.load(path,sample_rate)
 
 plt.figure(figsize=(9, 3))
-librosa.display.waveplot(x)
+# librosa.display.waveplot(log_S)
+print(delta2_mfcc[0][0:10])
+plt.plot(delta2_mfcc[0])
+# print(type(x), type(delta2_mfcc))
 
 # librosa.display.specshow(delta2_mfcc, x_axis='time')
 
@@ -77,4 +102,5 @@ librosa.display.waveplot(x)
 # plt.title('MFCC')
 # plt.colorbar()
 # plt.tight_layout()
+# plt.savefig("result.png")
 plt.show()
